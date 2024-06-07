7b68c0917fb087f317c2ca86ea05903ce08a36a6
Colin Qiao <colinqiao007@gmail.com>
2024-06-07 16:57:04 +0800

    fix

--- state-tax/state-tax-form-crawler/src/lambda_function.py
+++ state-tax/state-tax-form-crawler/src/lambda_function.py
@@ -1,12 +1,17 @@
 import json
+import requests
 
 
 def lambda_handler(event, context):
 
     print("Received event:", json.dumps(event, indent=2))
 
-    # Return a 200 status with a message
+    response = requests.get('https://api.github.com/rate_limit', timeout=5)
+    data = response.json()
+
+    print("Response: ", data)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(data)
     }
