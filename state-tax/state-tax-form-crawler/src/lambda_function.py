87e6c0e9ac7eafc61d9449fb8a92d5e9f87f97ea
Colin Qiao <colinqiao007@gmail.com>
2024-06-11 23:56:36 +0800

    update

--- state-tax/state-tax-form-crawler/src/lambda_function.py
+++ state-tax/state-tax-form-crawler/src/lambda_function.py
@@ -6,7 +6,7 @@ import requests
 
 def lambda_handler(event, context):
 
-    print("Prod:", json.dumps(event, indent=2))
+    print("master:", json.dumps(event, indent=2))
 
     response = requests.get('https://api.github.com/rate_limit', timeout=5)
     data = response.json()
